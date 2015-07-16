ae78feadbd0ebb4f07cbc0cb0ec710d2f669257b
tuanpham <anhtuanhnh@gmail.com>
2015-07-15 21:27:06 -0700

    Layout-ExplorePhoto

---
+++ client/app/modules/explore/controllers/photo.explore.ctrl.js
@@ -0,0 +1,54 @@
+/**
+ * Created by tuanpham on 7/15/15.
+ */
+var app=angular.module('com.module.explore');
+
+app.controller('modal_show_photo', function ($scope, $modal, $log) {
+
+  $scope.open = function (photo) {
+
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      resolve: {
+        items: function () {
+          return photo;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+});
+app.controller('hideMenu', function($scope) {
+  $scope.show = false;
+});
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
+app.controller('PopoverDemoCtrl', function ($scope) {
+  $scope.dynamicPopover = {
+    content: 'Hello, World!',
+    templateUrl: 'myPopoverTemplate.html',
+    title: 'Title'
+  };
+});
