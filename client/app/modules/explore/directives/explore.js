9cc526b271e0e382c62ea89ff5f8ecb83f10001b
vonguyenleduy <duylenguyen.vo@gmail.com>
2015-07-17 21:00:59 +0700

    fix bug

--- client/app/modules/explore/directives/explore.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('com.module.explore')
-  .directive('explore', function(User) {
-    return {
-      templateUrl:'modules/explore/views/app.html',
-      restrict: 'E'
-    };
-  });
