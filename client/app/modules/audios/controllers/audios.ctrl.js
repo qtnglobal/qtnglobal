06869bbe947d6f44805180ad45216c2f14b4ba9a
minh <minhhuynh261096@gmail.com>
2015-07-18 11:29:20 +0700

    fix audio

--- client/app/modules/audios/controllers/audios.ctrl.js
+++ client/app/modules/audios/controllers/audios.ctrl.js
@@ -74,19 +74,9 @@ angular.module('com.module.audios')
       type: 'text',
       label: gettextCatalog.getString('Title'),
       required: true
-    }, {
-      key: 'content',
-      type: 'textarea',
-      label: gettextCatalog.getString('Content'),
-      required: true
-    }/*, {
-      key: 'url',
-      type: 'text',
-      label: gettextCatalog.getString('Url'),
-      required: false
-    }*/];
+    }];
 
-    $scope.formFieldsUrl = [{
+    $scope.formFieldsContent = [{
       key: 'content',
       type: 'textarea',
       label: gettextCatalog.getString('Add a description, if you like'),
@@ -102,7 +92,7 @@ angular.module('com.module.audios')
 
     $scope.upload = function(item){
       if($scope.audio.ownerId === currentUser.id){
-        $scope.audio.url = CoreService.env.apiUrl+ '/containers/files/download/'+item.file.name;
+        $scope.audio.linkMp3 = CoreService.env.apiUrl+ '/containers/files/download/'+item.file.name;
         $scope.audio.cover = './images/mucsicIcon.png';
         console.log(item.file.name);
         Audio.upsert($scope.audio, function() {
@@ -120,54 +110,60 @@ angular.module('com.module.audios')
       }
     };
 
-    $scope.onSubmit = function(url) {
-          if($scope.audio.ownerId === currentUser.id){
-            if(url.match(/mp3.zing.vn/)){
-              var songID = url.substr(-13,8);
-              $http.get('http://api.mp3.zing.vn/api/mobile/song/getsonginfo?keycode=fafd463e2131914934b73310aa34a23f&requestdata={"id":"'+songID+'"}&jsonp=JSON_CALLBACK')
-                .success(function(data) {
-                  console.log(data);
-                  $scope.title = data.title;
-                  $scope.audio.artist = data.artist;
-                  $scope.audio.genre = data.genre_name;
-                  $scope.audio.cover = 'http://image.mp3.zdn.vn/' + data.album_cover;
-                  $scope.audio.composer = data.composer;
-                  $scope.audio.link_mp3 = data.link_download[128];
-                  $scope.audio.title = $scope.title;
-                  $scope.audio.url = url;
-                  Audio.upsert($scope.audio, function() {
-                    CoreService.toastSuccess(gettextCatalog.getString('Audio saved'),
-                     gettextCatalog.getString('Your audio is safe with us!'));
-                    $state.go('^.list');
-                  }, function(err) {
-                    console.log(err);
-                  });
-                });
-            } else if (url.match(/soundcloud/)) {
-              $http.get('http://api.soundcloud.com/resolve.json?url='+url+'&client_id=769247ada809c2e2640c6962c4017a9f')
-                .success(function(data){
-                  console.log(data);
-                  $scope.title = data.title;
-                  $scope.audio.artist = data.user.username;
-                  $scope.audio.cover = data.artwork_url.replace('large','t250x250');
-                  $scope.audio.link_mp3 = data.stream_url+'?client_id=769247ada809c2e2640c6962c4017a9f';
-                  $scope.audio.title = $scope.title;
-                  $scope.audio.url = url;
-                  Audio.upsert($scope.audio, function() {
-                    CoreService.toastSuccess(gettextCatalog.getString('Audio saved'),
-                      gettextCatalog.getString('Your audio is safe with us!'));
-                    $state.go('^.list');
-                  }, function(err) {
-                    console.log(err);
-                  });
-                });
-            }
-          }
-            else{
-              CoreService.alertWarning('May be you do not have permission to do this stuff','Please ask admin for permission');
-              $state.go('^.list');
-            }
+    $scope.getDataZing = function(data){
+      var title = data.match(/<title>(.*?)<\/title>/g);
+      var embed = data.match(/<link rel="video_src" href="(.*?)"/g);
+      var cover = data.match(/<meta property="og:image" content="(.*?)"/g);
+      var getXml = data.match(/data-xml="(.*?)"/g);
+      var xml = getXml[0].slice(10,-1);
+      var linkDownload = xml.replace('mp3.zing.vn/xml/song-xml/','api.mp3.zing.vn/api/mobile/source/song/');
+      $scope.audio.title = title[0].slice(7,-8);
+      $scope.audio.linkMp3 = linkDownload;
+      $scope.audio.cover = cover[0].slice(35,-1);
+      Audio.upsert($scope.audio, function() {
+        CoreService.toastSuccess(gettextCatalog.getString('Audio saved'),
+          gettextCatalog.getString('Your audio is safe with us!'));
+        $state.go('^.list');
+      }, function(err) {
+        console.log(err);
+      });
+    }
 
-      };
+    $scope.getDataSoundcloud = function(data){
+      $scope.audio.title = data.title;
+      $scope.audio.artist = data.user.username;
+      $scope.audio.cover = data.artwork_url.replace('large','t250x250');
+      $scope.audio.linkMp3 = data.stream_url+'?client_id=769247ada809c2e2640c6962c4017a9f';
+      Audio.upsert($scope.audio, function() {
+        CoreService.toastSuccess(gettextCatalog.getString('Audio saved'),
+          gettextCatalog.getString('Your audio is safe with us!'));
+        $state.go('^.list');
+      }, function(err) {
+        console.log(err);
+      });
+    }
+
+    $scope.onSubmit = function(url) {
+      if($scope.audio.ownerId === currentUser.id){
+        if(url.match(/mp3.zing.vn\//)){
+          $http.get(url)
+            .success(function(data) {
+              $scope.audio.url = url;
+              $scope.getDataZing(data);
+            });
+        } else if (url.match(/soundcloud/)) {
+          $http.get('http://api.soundcloud.com/resolve.json?url='+url+'&client_id=769247ada809c2e2640c6962c4017a9f')
+            .success(function(data){
+              $scope.audio.url = url;
+              $scope.getDataSoundcloud(data);
+            });
+        }
+      }
+      else{
+        CoreService.alertWarning('May be you do not have permission to do this stuff','Please ask admin for permission');
+        $state.go('^.list');
+      }
+
+    };
 
   });
