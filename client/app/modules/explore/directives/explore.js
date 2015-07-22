74c0b8fa44c9968915d3152546544b4406e8016c
hung <hcaelb123@gmail.com>
2015-07-22 12:56:11 +0700

    fix layout bug

--- client/app/modules/explore/directives/explore.js
+++ client/app/modules/explore/directives/explore.js
@@ -1,9 +1,21 @@
 'use strict';
 
-angular.module('com.module.explore')
-  .directive('explore', function(User) {
+var app= angular.module('com.module.explore');
+  app.directive('explore', function(User) {
     return {
       templateUrl:'modules/explore/views/app.html',
       restrict: 'E'
     };
   });
+app.directive('container',function($timeout){
+  return{
+    restrict:'A',
+    link:function(scope, element, attrs){
+      $timeout(function () {
+        var iso = new Isotope(element[0], {
+          itemSelector: '.boxEx3'
+        });
+      }, 200);
+    }
+  }
+});
