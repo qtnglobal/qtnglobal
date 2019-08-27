223b7d938b00acb0eb4c8603eb8713ff631e298e
Jason Quach <thuanqh@gmail.com>
2019-08-27 18:18:04 +0700

    Add logo and 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,28 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {}
-    }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `QTN Global`,
+        short_name: `QTN`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#cb2ba7`,
+        display: `standalone`,
+        icon: `src/assets/images/qtnglobal.png`
+      }
+    },
+    `gatsby-plugin-offline`
   ]
 };
